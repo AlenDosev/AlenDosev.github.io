b2cf702f1f1279a92036e8ceca5b96992c6b1ee2
Alen Dosev <alendosev@hotmail.com>
2016-11-08 20:20:33 +0100

    test

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
 
 $(document).ready(function(){
+    // var swiper = new Swiper('.swiper-container', {
+    //         pagination: '.swiper-pagination',
+    //         slidesPerView: 1,
+    //         paginationClickable: true,
+    //         spaceBetween: 30,
+    //         keyboardControl: true,
+    //         nextButton: '.swiper-button-next',
+    //         prevButton: '.swiper-button-prev',
+    //         spaceBetween: 0
+    //     });
+});
 
+$(window).scroll(function(){
+    if($(window).scrollTop() + $(window).height() > $("#test").offset().top){
+        $(".bla").css('opacity','1');
+        $(".bla").css('margin-top','0px');
+    }
+
+  
 });
